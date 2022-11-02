c960cabbd59b4989cd98155f544406a5a1a6855c
minhtumn <minngu@ucdavis.edu>
2022-11-02 02:15:10 -0700

    sort of did 4?

--- ecs152-p2.py
+++ ecs152-p2.py
@@ -2,10 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pickle import FALSE, TRUE
 import sys
 import socket
 import binascii
-import bitarray
+# import bitarray
 
 def get_type(type):
     types = [
@@ -163,12 +164,8 @@ def parse(message):
     response = []
 
     ID = message[0:4]
-    flags = message[4:8]
-    QDCOUNT = message[8:12]
-    ANCOUNT = message[12:16]
-    NSCOUNT = message[16:20]
-    ARCOUNT = message[20:24]
 
+    flags = message[4:8]
     parameters = bin(int(flags, 16)).zfill(16)
 
     QR = parameters[0:1]
@@ -180,15 +177,138 @@ def parse(message):
     Z = parameters[9:12]
     RCODE = parameters[12:16]
 
+    QDCOUNT = message[8:12]
+    ANCOUNT = message[12:16]
+    NSCOUNT = message[16:20]
+    ARCOUNT = message[20:24]
+
+
     header = ["ID", "QR", "OPCODE", "AA", "TC", "RD", "RA", "Z", "RCODE", "QDCOUNT", "ANCOUNT", "NSCOUNT", "ARCOUNT"]
     header_values = [ID, QR, OPCODE, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT]
 
     for i in range(0, len(header)):
         response.append(header[i] + ": " + header_values[i])
 
-    print(response)
 
     # question
+    qlength = message[24:26]
+    qname = ""
+    qcurrent = ""
+
+    end = 26 + int(qlength) * 2
+
+    qname = message[26:end]
+
+    q_string = bytes.fromhex(qname)
+    q_string = q_string.decode("ascii")
+    q_string = q_string
+
+    print(q_string)
+    
+    start = end
+    end = start + 2
+
+    while message[start:end] != "00":
+        qlength = message[start:end]
+        p_end = end + int(qlength) * 2
+        qname = message[end:p_end]
+
+        qname = bytes.fromhex(qname)
+        qname = qname.decode("ascii")
+        q_string = q_string + "." + qname
+        
+        start = p_end
+        end = p_end + 2
+
+    start = p_end
+    end = p_end + 4
+    
+    qtype = message[start:end]
+
+    start = end
+    end = end + 4
+
+    qclass = message[start:end]
+
+    #answer
+    start = end
+    end = end + 4
+
+
+
+    aname = message[start:end]
+
+    start = end
+    end = end + 4
+
+    atype = message[start:end]
+
+    if atype == "0000":
+
+        start = end
+        end = end + 4
+
+        aclass = message[start:end]
+
+        start = end
+        end = end + 8
+
+        attl = message[start:end]
+
+        start = end
+        end = end + 4
+
+        rdlength = message[start:end]
+        print(rdlength)
+
+        start = end
+        end = end + int(rdlength) * 2
+
+        rd = message[start:end]
+        ip = ""
+
+        count = 0
+        count_end = 2
+
+        while count != int(rdlength):
+            current = rd[count:count_end]
+            temp = bytes.fromhex(current)
+            if count_end != int(rdlength):
+                ip = ip + temp.decode("ascii") + "."
+
+            count = count_end
+            count_end = count_end + 2
+        
+        print(ip)
+
+
+
+    print(q_string)
+    
+    # while message[tracker:tracker+2] != "00" or qlength != 0:
+    #     print(tracker)
+    #     for i in range(int(qlength)):
+    #         if message[tracker:tracker+2] != "00": 
+    #             qcurrent = qcurrent + message[tracker:tracker+2]
+    #             tracker += 2
+    #         else:
+    #             print("reached end")
+    #             break
+    #     qname = bytes.fromhex(qcurrent)
+    #     qname = qname.decode("ascii")
+    #     qcurrent = ""
+    #     if message[tracker:tracker+2] != "00": 
+    #         qname = qname + "."
+    #         qlength = message[tracker:tracker+2]
+
+
+    
+
+
+
+        
+
+    
 
 
 # Press the green button in the gutter to run the script.
